df5467a6be97e8444750509f9019ec661bc3bb78
daniel <gdanielch9@gmail.com>
2017-02-28 10:31:08 +0100

    EventService tests added

---
+++ Tests/Services/EventServiceTest.cs
@@ -0,0 +1,55 @@
+﻿using Moq;
+using MyNote.Dtos;
+using MyNote.Entities;
+using MyNote.Repositories;
+using MyNote.Services;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Services
+{
+    [TestFixture]
+    class EventServiceTest
+    {
+        private Mock<IEventsRepository> _eventsRepositoryMock;
+        private EventService _sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _eventsRepositoryMock = new Mock<IEventsRepository>();
+            _sut = new EventService(_eventsRepositoryMock.Object);
+        }
+
+        [Test]
+        public void Given_ExchangeService_When_GetShowEventViewModelList_Then_Return_2ElementList()
+        {
+            // given
+            _eventsRepositoryMock.Setup(x => x.GetEventList()).Returns(new List<Event>()
+            {
+                new Event(),
+                new Event()
+            });
+            // when
+            var result = _sut.GetShowEventViewModelList();
+            // then
+            Assert.AreEqual(2, result.Count);
+        }
+
+        [Test]
+        public void Given_Exchange_Service_When_GetEventList_And_List_Is_Empty_Then_Return_Empty_NotNULL_List()
+        {
+            // given
+            _eventsRepositoryMock.Setup(x=>x.GetEventList()).Returns(new List<Event>());
+            // when
+            var result = _sut.GetShowEventViewModelList();
+            // then
+            Assert.IsNotNull(result);
+            Assert.IsEmpty(result);
+        }
+    }
+}
